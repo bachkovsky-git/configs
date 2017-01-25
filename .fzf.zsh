9ff8dddf072f97da43310840617426682e5b5df3
Mikhail Tsirikov <t.bachkovsky@gmail.com>
2017-01-25 15:42:04 +0300

    add psql commit, remove vpn commits

---
+++ .fzf.zsh
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/mike/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/mike/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == */home/mike/.fzf/man* && -d "/home/mike/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/mike/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/mike/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/mike/.fzf/shell/key-bindings.zsh"
+
